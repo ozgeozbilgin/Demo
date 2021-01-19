7c94c44ff6da2c7c202a957aaedfac5e4d3b1870
ozgeozbilgin95@gmail.com <ozgeozbilgin95@gmail.com>
2021-01-19 12:56:54 +0300

    Add project files.

---
+++ Demo/MusteriYoneticisi.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Demo
+{
+    class MusteriYoneticisi
+    {
+        public void Ekle(Musteri musteri)
+        {       
+
+            Console.WriteLine(musteri.Ad + " " + musteri.Soyad + "  adlı müşteri listeye eklendi");
+
+        }
+        public void Listele(Musteri[] musteriler)
+        {
+            foreach (Musteri musteri in musteriler)
+            {
+
+           
+                Console.WriteLine(musteri.Id);
+                Console.WriteLine(musteri.Ad);
+                Console.WriteLine(musteri.Soyad);
+                Console.WriteLine("-------------------------------------------------");
+
+            }
+
+        }
+        public void Sil(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Ad+ " " +musteri.Soyad+" adlı müşteri silindi");
+        }
+    }
+}
---
+++ Demo/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 10;
+            musteri1.Ad = "Özge";
+            musteri1.Soyad = "Özbilgin";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 13;
+            musteri2.Ad = "Melek";
+            musteri2.Soyad = "Özbilgin";
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Id = 27;
+            musteri3.Ad = "Öznur";
+            musteri3.Soyad = "Özbilgin";
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2, musteri3 };
+
+           
+           
+            //müşteri eklendi
+            MusteriYoneticisi musteriYoneticisi = new MusteriYoneticisi();
+            musteriYoneticisi.Ekle(musteri1);
+            musteriYoneticisi.Ekle(musteri2);
+            musteriYoneticisi.Ekle(musteri3);
+
+            //müsteri listelendi
+            musteriYoneticisi.Listele(musteriler);
+
+            //silindi
+            musteriYoneticisi.Sil(musteri1);
+            musteriYoneticisi.Sil(musteri2);
+            musteriYoneticisi.Sil(musteri3);
+
+
+
+
+        }
+    }
+}
